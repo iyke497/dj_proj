c70de2da7c8581fe420294d80eda78bdb7ae9eb2
Iyke <iyke497@gmail.com>
2024-08-25 05:38:27 +0100

    Writing more views

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-
 from .models import Question
 
-admin.site.register(Question)
+#admin.site.register(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ["question_text"]
+
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,4 +6,7 @@ from . import views
 #Path function is passed 4 arguments 2 required {route and view} and 2 optional kwargs and name
 urlpatterns = [
 	path("", views.index, name="index"),
+	path("<int:question_id>/", views.detail, name="detail"),
+	path("<int:question_id>/results/", views.results, name="results"),
+	path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
